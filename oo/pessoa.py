87c6c914e2a75b0ccf26639a9d5a72b3706da1c0
JuniorD-Isael <isaeldjunior@gmail.com>
2024-12-09 21:51:03 -0300

    criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,21 @@
 class Pessoa:
-    # Metodo
-
-    def __init__(self, nome=None, idade=31):
+    def __init__(self, *filhos,nome=None, idade=31):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f"Olá! {id(self)}"
 
-    # Atributos de dado
 
 if __name__ == '__main__':
-    p = Pessoa("Isael")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = "Junior"
-    print(p.nome)
-    print(p.idade)
+    caio = Pessoa(nome="Caio")
+    isael = Pessoa(caio,nome="Isael")
+    print(Pessoa.cumprimentar(isael))
+    print(id(isael))
+    print(isael.cumprimentar())
+    print(isael.nome)
+    print(isael.idade)
+
+    for filho in isael.filhos:
+        print(filho.nome)
