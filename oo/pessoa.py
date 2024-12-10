930ff2bc3d97e6c303f0bd1bad4d58974d39563a
JuniorD-Isael <isaeldjunior@gmail.com>
2024-12-09 22:02:22 -0300

    criado e removido atributo dinâmico do objeto do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,7 +2,10 @@ class Pessoa:
     def __init__(self, *filhos,nome=None, idade=31):
         self.idade = idade
         self.nome = nome
+
+        # Atributo Complexo
         self.filhos = list(filhos)
+
 
     def cumprimentar(self):
         return f"Olá! {id(self)}"
@@ -19,3 +22,11 @@ if __name__ == '__main__':
 
     for filho in isael.filhos:
         print(filho.nome)
+
+    # Atributo Dinamico
+    isael.sobrenome = "Junior"
+    del isael.filhos
+
+    # Os atributos de instância ficam presente no atributo especial __dict__
+    print(isael.__dict__)
+    print(caio.__dict__)
