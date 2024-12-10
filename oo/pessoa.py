d2618bb72617a0503481c5b2beee4e734d7f1262
JuniorD-Isael <isaeldjunior@gmail.com>
2024-12-09 21:42:33 -0300

    criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,21 @@
 class Pessoa:
+    # Metodo
+
+    def __init__(self, nome=None, idade=31):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f"Olá! {id(self)}"
 
+    # Atributos de dado
+
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("Isael")
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Junior"
+    print(p.nome)
+    print(p.idade)
